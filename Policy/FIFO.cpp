eb0c801b0d819d10c017256b5b0cdd46f8da01cb
Soojal1807 <2004soojal@gmail.com>
2026-07-03 23:17:26 +0530

    Implemented : FIFO policy

---
+++ Policy/FIFO.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+#include <unordered_set>
+
+class Cache {
+    int cap;
+    std::queue<int> q;
+    std::unordered_set<int> s;
+
+public:
+    Cache(int c) : cap(c) {}
+
+    void get(int k) {
+        if (s.find(k) == s.end()) return;
+    }
+
+    void put(int k) {
+        if (s.find(k) != s.end()) return;
+        if (s.size() == cap) {
+            s.erase(q.front());
+            q.pop();
+        }
+        q.push(k);
+        s.insert(k);
+    }
+};
+
+
+int main() {
+    Cache c(2);
+    c.put(1);
+    c.put(2);
+    c.put(3);
+    return 0;
+}
